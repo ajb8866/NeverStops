c1a820b4094f6737fd35e3ae2e48e3dd91bffd99
Averi Bates <Averi.J.Bates@ou.edu>
2020-10-21 16:34:07 -0500

    why won't anything work

---
+++ eclipse-workspace/NeverStops/src/SuperStack.java
@@ -0,0 +1,26 @@
+
+public class SuperStack<T> {
+	private T[] items;
+	private int top;
+	public SuperStack(int size)
+	{
+		items = (T[]) new Object[size];
+	}
+	public boolean isEmpty()
+	{
+		return true;
+	}
+	public boolean isFull()
+	{
+		return true ;
+	}
+	
+	public boolean push (T items)
+	{
+		return true;
+	}
+	
+	
+	
+
+}
