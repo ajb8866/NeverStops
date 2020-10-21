0071f9315c62436f71fa7c902877ba5833c5888e
Averi Bates <Averi.J.Bates@ou.edu>
2020-10-21 17:00:24 -0500

    gitbash is the worst

---
+++ eclipse-workspace/NeverStops/src/SuperStackTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SuperStackTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testIsEmpty()
+	{
+		SuperStack<Integer> stack = new SuperStack<>(1);
+		boolean resutls = stack.isEmpty();
+		assertTrue 
+	}
+
+}
